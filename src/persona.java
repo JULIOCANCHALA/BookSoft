9325e95303cf54ba7fdb6c241db426b82f7d1473
Julio <Julio@JulioPC>
2015-09-02 17:52:09 -0500

    Version 1.0 Version de  prueba de conexion a HitGub

---
+++ src/persona.java
@@ -0,0 +1,17 @@
+/**
+ *
+ * @author Julio
+ */
+public class persona {
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+    private String cedula;
+    
+    
+}
